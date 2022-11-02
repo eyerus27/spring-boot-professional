b055f856bb6338123af621daf8d0c7b324a1fd9f
Eyerusalem Gebregziabher <ewoldetnsay@yahoo.com>
2022-11-02 14:34:38 -0700

    make test fail

--- src/test/java/com/example/demo/Demo1ApplicationTests.java
+++ src/test/java/com/example/demo/Demo1ApplicationTests.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +9,8 @@ class Demo1ApplicationTests {
 
     @Test
     void contextLoads() {
+        Assertions.fail("Testing");
     }
 
 }
+   

7b0e1a0600cdddaadf11ea6184aeb246a91c6684
Eyerusalem Gebregziabher <ewoldetnsay@yahoo.com>
2022-11-07 16:10:47 -0800

    testing

--- src/test/java/com/example/demo/Demo1ApplicationTests.java
+++ src/test/java/com/example/demo/Demo1ApplicationTests.java
@@ -1,11 +1,16 @@
 package com.example.demo;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class Demo1ApplicationTests {
 
-
+    @Test
+    void contextLoads() {
+       // Assertions.fail("testing");
+    }
 
 }
    

3faad3493a983d44b58216e0a3e954cbdf794d8f
Eyerusalem Gebregziabher <ewoldetnsay@yahoo.com>
2022-11-19 13:58:53 -0800

    Gender_Updated (#15)
    * testing
    
    * testing
    
    * testing
    
    * testing agian
    
    * changed package.json
    
    * Testing without Assertion fail
    
    * any changes
    
    * updated pom
    
    * updated pom
    
    * updated pom
    
    * modified pom and test
    
    * modified pom
    
    * modified pom again
    
    * modified test again
    
    * modified application-dev.properties file
    
    * modified pom file
    
    * updatedd pom file
    
    * updatedd pom file
    
    * student class updated in add-footer
    
    * pom updated againnn
    
    * updated student class

--- src/main/java/com/example/demo/student/Student.java
+++ src/main/java/com/example/demo/student/Student.java
@@ -32,9 +32,9 @@ public class Student {
     @Email()
     @Column(nullable = false,unique = true)
     private String email;
-    @NotNull
-    @Enumerated(EnumType.STRING)
-    @Column(nullable = false,unique = false)
+   @NotNull
+   @Enumerated(EnumType.STRING)
+   @Column(nullable = false)
 
     private Gender gender;
 

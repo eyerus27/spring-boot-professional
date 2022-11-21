9331bff678cd56f59069d34fb7e6089afddc4ae6
Eyerusalem Gebregziabher <ewoldetnsay@yahoo.com>
2022-11-21 15:22:58 -0800

    Testing branch (#16)
    * testing
    
    * testing
    
    * testing
    
    * testing agian
    
    * changed package.json
    
    * Testing without Assertion fail
    
    * any changes
    
    * updated pom
    
    * updated pom
    
    * updated pom
    
    * modified pom and test
    
    * modified pom
    
    * modified pom again
    
    * modified test again
    
    * modified application-dev.properties file
    
    * modified pom file
    
    * updatedd pom file
    
    * updatedd pom file
    
    * testing branch pushing
    
    * pom updated
    
    * pom updated in testing
    
    * pom updated again
    
    * pom updated againnn
    
    * updated student gender entity

--- src/main/java/com/example/demo/student/Student.java
+++ src/main/java/com/example/demo/student/Student.java
@@ -32,9 +32,10 @@ public class Student {
     @Email()
     @Column(nullable = false,unique = true)
     private String email;
-   @NotNull
-   @Enumerated(EnumType.STRING)
-   @Column(nullable = false)
+    @NotNull
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+
 
     private Gender gender;
 
